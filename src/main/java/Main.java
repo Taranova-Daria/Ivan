d1c958ac5a228fc112e4378fc0f74ba63dd3e976
Dasha <taranova_dasha@mail.ru>
2023-07-01 13:01:38 +0300

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+        System.out.println("Имя: " + post.name);
+        System.out.println("Паспорт: " + post.passport);
+        System.out.println("Отчество: " + post.patronymic);
+        System.out.println("Телефон: " + post.phone);
+        System.out.println("Фамилия: " + post.surname);
+        System.out.println("Подписка: " + post.subscription);
+        System.out.println("Дата рождения: " + post.birthday.day + "." + post.birthday.month + "." + post.birthday.year);
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,15 @@
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+    public static class Birthday {
+        public int day;
+        public int month;
+        public int year;
+    }
+}
